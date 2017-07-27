2fd1bed51babae705a90842c66e340a1fe9c48e2
Darima Mylzenova <d.mylzenova@analysiscentre.ru>
2017-07-27 21:54:22 +0300

    final changes

--- task_01/linear.py
+++ task_01/linear.py
@@ -31,32 +31,35 @@ class MyBatch(Batch):
         y_cup = tf.add(tf.matmul(x_features, weights), bias)
         cost = tf.reduce_mean(tf.square(y_cup - y_target))
         training_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
         return training_step, cost, x_features, y_target, y_cup
+    
+    def train_any(self, model, sess, my_cost_history):
+        ''' Train any regression on the batch '''
+        training_step, cost, x_features, y_target = model[:-1]
+        sess.run(training_step, feed_dict={x_features:self.features, y_target:self.labels})
+        my_cost_history.append(sess.run(cost, feed_dict={x_features:self.features, y_target:self.labels}))
+        return my_cost_history
+
+    def predict_any(self, model, sess, y_pred):
+        '''Predict target for any model'''
+        x_features = model[2]
+        y_cup = model[4]
+        y_pred[:] = sess.run(y_cup, feed_dict={x_features:self.features})
+
 
     @action(model='linear_regression')
-    def train_linear(self, model, my_sess, my_cost_history):
+    def train_linear(self, model, sess, my_cost_history):
         ''' Train linear regression on the batch '''
-        training_step, cost, x_features, y_target = model[:-1]
-        my_sess.run(training_step, feed_dict={x_features:self.features, y_target:self.labels})
-        my_cost_history.append(my_sess.run(cost, feed_dict={x_features:self.features, y_target:self.labels}))
+        self.train_any(model, sess, my_cost_history)
         return self
 
     @action(model='linear_regression')
-    def test_linear(self, model, sess):
-        ''' Test linear regression on the batch '''
-        x_features = model[2]
-        y_cup = model[4]
-        y_pred = sess.run(y_cup, feed_dict={x_features:self.features})
-        mse = tf.reduce_mean(tf.square(y_pred - self.labels))
-        print("MSE: %.4f" % sess.run(mse))
-        
-        axis = plt.subplots()[1]
-        axis.scatter(self.labels, y_pred)
-        axis.plot([self.labels.min(), self.labels.max()], [self.labels.min(), self.labels.max()], 'k--')
-        axis.set_xlabel('Measured')
-        axis.set_ylabel('Predicted')
-        plt.show()
+    def test_linear(self, model, sess, y_true, y_pred, mse, x_features):
+        ''' Test batch '''
+        self.predict_any(model, sess, y_pred)
+        mse.append(sess.run(tf.reduce_mean(tf.square(y_pred - self.labels))))
+        y_true[:] = self.labels
+        x_features[:] = self.features
         return self
 
     @model()
@@ -67,38 +70,39 @@ class MyBatch(Batch):
         y_target = tf.placeholder(tf.float32, [None, 1])
         weights = tf.Variable(tf.zeros([NUM_DIM, 1]))
         bias = tf.Variable(tf.zeros([1]))
-        
+
         wx_b = tf.add(tf.matmul(x_features, weights), bias)
 
         prob = tf.sigmoid(wx_b)
         y_cup = tf.sign(wx_b)
-        
+
         margin = tf.multiply(y_target, wx_b)
         cost = tf.reduce_mean(tf.log(tf.add(tf.ones([1]), tf.exp(-margin))))
         training_step = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+        acc = tf.contrib.metrics.accuracy(tf.cast(y_cup, tf.int32), tf.cast(y_target, tf.int32))
 
-        return training_step, cost, x_features, y_target, y_cup
+        return training_step, cost, x_features, y_target, y_cup, acc
 
     @action(model='logistic_regression')
-    def train_logistic(self, model, my_sess, my_cost_history):
+    def train_logistic(self, model, sess, my_cost_history, acc_history):
         ''' Train logistic regression on the batch '''
-        training_step, cost, x_features, y_target = model[:-1]
-        my_sess.run(training_step, feed_dict={x_features:self.features, y_target:self.labels})
-        my_cost_history.append(my_sess.run(cost, feed_dict={x_features:self.features, y_target:self.labels}))
+        # self.train_any(model, sess, my_cost_history)
+        training_step, cost, x_features, y_target = model[:-2]
+        acc = model[-1]
+        sess.run(training_step, feed_dict={x_features:self.features, y_target:self.labels})
+        my_cost_history.append(sess.run(cost, feed_dict={x_features:self.features, y_target:self.labels}))
+        
+        acc_history.append(sess.run(acc, feed_dict={x_features:self.features, y_target:self.labels}))
         return self
     
     @action(model='logistic_regression')
-    def test_logistic(self, model, my_sess):
+    def test_logistic(self, model, sess, acc):
         ''' Test logistic regression on the batch'''
-        x_features = model[2]
-        y_cup = model[4]
-        y_pred = my_sess.run(y_cup, feed_dict={x_features:self.features})
-        
+        y_pred = np.zeros([len(self.labels), 1])
+        self.predict_any(model, sess, y_pred)
         y_target_int = tf.cast(self.labels, tf.int32)
         y_pred_int = tf.cast(y_pred, tf.int32)
-        
-        acc = tf.contrib.metrics.accuracy(y_pred_int, y_target_int)
-        print("ACCURACY: %.4f" % my_sess.run(acc))
+        acc.append(sess.run(tf.contrib.metrics.accuracy(y_pred_int, y_target_int)))
         return self
 
     @model()
@@ -113,43 +117,31 @@ class MyBatch(Batch):
         log_input = tf.add(tf.matmul(x_features, weights), bias)
         cost = tf.reduce_mean(tf.nn.log_poisson_loss(y_target, log_input, compute_full_loss=False))
         
-        y_cup = tf.exp(log_input)
+        # y_cup = tf.exp(log_input)
             
         training_step = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-        return training_step, cost, x_features, y_target, y_cup
+        return training_step, cost, x_features, y_target, log_input, weights
 
 
     @action(model='poisson_regression')
-    def train_poisson(self, model, my_sess, my_cost_history):
+    def train_poisson(self, model, sess, my_cost_history):
         ''' Train poisson regression on the batch '''
-        training_step, cost, x_features, y_target = model[:-1]
-        my_sess.run(training_step, feed_dict={x_features:self.features, y_target:self.labels})
-        my_cost_history.append(my_sess.run(cost, feed_dict={x_features:self.features, y_target:self.labels}))
+        self.train_any(model[:-1], sess, my_cost_history)
         return self
 
 
     @action(model='poisson_regression')
-    def test_poisson(self, model, my_sess):
+    def test_poisson(self, model, sess, y_true, y_pred, weights):
         ''' Test poisson regression on the batch '''
-        cost = model[1]
-        x_features = model[2]
-        y_cup = model[-1]
-
-        print ('Y_CUP SHAPE ', y_cup.shape)
+        # y_pred = np.zeros([len(self.labels), 1])
+        self.predict_any(model[:-1], sess, y_pred)
+        # cost = model[1]
         
-        y_pred = my_sess.run(y_cup, feed_dict={x_features:self.features})
-        # y_pred_sm = tf.round(y_pred) + 1 if ? tf.round(y_pred) 0 : tf.round(y_pred) + 1 : tf.round(y_pred)
-        print (y_pred.shape, '@@@@@@@@@@')
+        # y_pred_int = sess.run(tf.cast(tf.round(y_pred) + 1, tf.int32))
 
-        y_pred_int = my_sess.run(tf.cast(tf.round(y_pred) + 1, tf.int32))
-
-
-        mse = tf.reduce_mean(tf.square(y_pred_int - self.labels))
-        # log_loss = tf.losses.log_loss(self.labels, y_pred_int, epsilon=1e-05)
-        # print ('PRED ', y_pred_int)
-        # print ('TRUE ', self.labels)
-        print ()
-        print ("MSE: %.4f" % my_sess.run(mse))
+        # mse = tf.reduce_mean(tf.square(y_pred_int - self.labels))
+        weights[:] = sess.run(model[-1], feed_dict={model[2]:self.features})
+        y_true[:] = self.labels
         return self
 
 
@@ -167,22 +159,23 @@ def load_poisson_data():
     xw = np.exp(np.dot(features, weights))
     labels = np.reshape(np.array([np.random.poisson(lmbd) for lmbd in xw]), [500, 1])
     labels.astype(np.float32)
-    return features, labels
+    weights = np.reshape(weights, [NUM_DIM, 1])
+    return weights, features, labels
 
 
-def load_random_data(blobs=False):
+def load_random_data(n_samples=500, blobs=False):
     ''' load some data '''
     if blobs:
-        features, labels = make_blobs(n_samples=500, n_features=13, random_state=1, centers=2)
+        features, labels = make_blobs(n_samples=n_samples, n_features=13, random_state=1, centers=2)
     else:
-        features, labels = make_classification(n_samples=500, n_features=13, random_state=1, n_classes=2, flip_y=0.005)
+        features, labels = make_classification(n_samples=n_samples, n_features=13, random_state=1, n_classes=2, flip_y=0.005)
     labels = np.reshape(labels, [labels.shape[0], 1])
     return features, labels
 
 
-def load_dataset(input_data):
+def load_dataset(n_samples):
     ''' create Dataset with given data '''
-    dataset = Dataset(index=np.arange(input_data[0].shape[0]), batch_class=MyBatch, preloaded=input_data)
+    dataset = Dataset(index=np.arange(n_samples), batch_class=MyBatch)    
     dataset.cv_split()
     return dataset
 
@@ -198,4 +191,40 @@ def preprocess_logistic_data(data):
     features, labels = data
     labels = 2*data[1] - np.ones((len(data[1]), 1), dtype=np.float32)
     return features, labels
+
+
+def plot_cost(cost_history):
+    plt.plot(range(len(cost_history)), cost_history)
+    plt.axis([0, len(cost_history), np.min(cost_history), np.max(cost_history)])
+    plt.show()
+
+
+def plot_test_linear(x_features, y_true, y_pred):
+    axis = plt.subplots()[1]
+    axis.scatter(y_true, y_pred)
+    axis.plot(np.sort(x_features[:, 0], y_pred, 'k--')
+    axis.plot(np.sort(x_features[:, 0], y_true)
+    axis.set_xlabel('x')
+    axis.set_ylabel('y')
+    plt.show()
+
+
+def plot_log_cost(sess, data, cost_history):
+    x_features = tf.placeholder(tf.float32, [None, None])
+    y_target = tf.placeholder(tf.float32, [None, 1])
+    weights = tf.placeholder(tf.float32, [13, 1])
+
+    log_input = tf.matmul(x_features, weights)
+    cost = tf.reduce_mean(tf.nn.log_poisson_loss(y_target, log_input, compute_full_loss=False))
+
+    origin_loss = sess.run(cost, feed_dict={weights:data[0], x_features:data[1], y_target:data[2]})
+
+    plt.plot(range(len(cost_history)), cost_history)
+    plt.plot(np.linspace(0, len(cost_history), len(cost_history)), [origin_loss]*len(cost_history), '-', color='g')
+    plt.axis([0, len(cost_history), np.min(cost_history), np.max(cost_history)])
+    plt.show()
+
+
+def compute_sample_var(labels):
+    return n/(n-1)*np.var(labels)
 
