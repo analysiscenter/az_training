e8efb883b0b58b4049cabf636cea2ff6c895ecda
anton-br <ziliboba753@gmail.com>
2017-10-24 11:40:11 +0300

    new my_batch in task_02

--- task_02/my_batch.py
+++ task_02/my_batch.py
@@ -1,9 +1,7 @@
 """ Custom batch class for storing mnist batch and models
 """
 import sys
-import os
 
-import blosc
 import numpy as np
 
 sys.path.append('..')
@@ -32,37 +30,6 @@ class MnistBatch(ImagesBatch):
         return 'images', 'labels'
 
     @action
-    def load(self, src, fmt='blosc', components=None, *args, **kwargs):
-        """ Load mnist pics with specified indices
-
-        Parameters
-        ----------
-        src: str or numpy array
-        if fmt='blosc' then src is a path to dir with blosc-packed
-                mnist images and labels are stored.
-        if fmt='ndarray' - this is a tuple with arrays of images and labels
-
-        fmt: 'blosc' or 'ndarray'
-        Format of source
-
-        components: dict, optional
-        Not used """
-        _, _, _ = args, kwargs, components
-        if fmt == 'blosc':
-            # read blosc images, labels
-            with open(os.path.join(src, 'mnist_pics.blk'), 'rb') as file:
-                self.images = blosc.unpack_array(file.read())[self.indices]
-
-            with open(os.path.join(src, 'mnist_labels.blk'), 'rb') as file:
-                self.labels = blosc.unpack_array(file.read())[self.indices]
-        elif fmt == 'ndarray':
-            all_images, all_labels = src
-            self.images = all_images[self.indices]
-            self.labels = all_labels[self.indices]
-
-        return self
-
-    @action
     @inbatch_parallel(init='init_func', post='post_func', target='threads')
     def shift_flattened_pic(self, ind, max_margin=8):
         """ Apply random shift to a flattened pic
@@ -86,7 +53,7 @@ class MnistBatch(ImagesBatch):
         left_lower = np.random.randint(2 * max_margin, size=2)
         slicing = (slice(left_lower[0], left_lower[0] + 28),
                    slice(left_lower[1], left_lower[1] + 28))
-        return padded[slicing].reshape(-1)
+        return padded[slicing].reshape(-1, 28, 28)
 
     def post_func(self, list_of_res):
         """ Concat outputs from shift_flattened_pic
@@ -97,7 +64,7 @@ class MnistBatch(ImagesBatch):
         if any_action_failed(list_of_res):
             raise Exception("Something bad happend")
         else:
-            self.images = np.stack(list_of_res)
+            self.images = np.stack(list_of_res).reshape(-1, 28, 28)
             return self
 
     def init_func(self):
