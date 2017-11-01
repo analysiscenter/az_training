a84f0d3428b5822bea286ad0771ed3e38071ff56
anton-br <ziliboba753@gmail.com>
2017-11-01 20:33:34 +0300

    Retrain task_02 and add is_training to model

--- task_02/simple_conv_model.py
+++ task_02/simple_conv_model.py
@@ -19,9 +19,9 @@ class ConvModel(TFModel):
         image = tf.placeholder(tf.float32, [None, 28, 28], name='input')
         image = tf.reshape(image, shape=[-1, 28, 28, 1])
         net = conv_block(2, image, filters=16, kernel_size=(7, 7), layout='cpna', pool_size=(3, 3),
-                         pool_strides=(2, 2), is_training=training)
+                         pool_strides=(2, 2))
         net = conv_block(2, net, filters=32, kernel_size=(5, 5), layout='cpna', pool_size=(3, 3),
-                         pool_strides=(2, 2), is_training=training)
+                         pool_strides=(2, 2))
         net = conv_block(2, net, filters=64, kernel_size=(3, 3), layout='cpna', pool_size=(2, 2),
                          pool_strides=(1, 1), dropout_rate=keep_prob, is_training=training)
 
