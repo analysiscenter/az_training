a1a9f07f85bbec81140e1293b950c6894816686c
Darima Mylzenova <d.mylzenova@analysiscenter.com>
2017-11-20 15:33:17 +0300

    update to latest version of dataset

--- darima_mylzenova/task_01/generate_data.py
+++ darima_mylzenova/task_01/generate_data.py
@@ -1,19 +1,12 @@
-import sys
+"""data generation functions for regression models"""
 import numpy as np
-
-import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_classification
 
-NUM_DIM = 13
-
-sys.path.append("..")
-from dataset import Dataset
-
-def load_linear_data(num_samples=500):
-    ''' load some data '''
-    features = np.random.random_sample([num_samples, NUM_DIM])
-    weights = np.random.random_sample([NUM_DIM])
+def load_linear_data(num_dim, num_samples=500):
+    ''' generate linearly dependent data '''
+    features = np.random.random_sample([num_samples, num_dim])
+    weights = np.random.random_sample([num_dim])
     x_mult_w = np.dot(features, weights)
 
     labels = np.random.normal(loc=0.0, scale=0.1, size=num_samples) + x_mult_w
@@ -21,56 +14,25 @@ def load_linear_data(num_samples=500):
     return features, labels
 
 
-def load_poisson_data():
-    '''generate poisson data'''
-    features = np.random.random_sample([500, NUM_DIM])
-    weights = np.random.random_sample([NUM_DIM])
+def load_poisson_data(num_dim, num_samples=500):
+    '''generate data for poisson regression'''
+    features = np.random.random_sample([num_samples, num_dim])
+    weights = np.random.random_sample([num_dim])
     bias = np.random.random(1)
     x_mult_w = np.exp(np.dot(features, weights) + bias)
-    labels = np.reshape(np.random.poisson(x_mult_w), [500, 1])
+    labels = np.reshape(np.random.poisson(x_mult_w), [num_samples, 1])
     labels.astype(np.float32)
-    weights = np.reshape(weights, [NUM_DIM, 1])
+    weights = np.reshape(weights, [num_dim, 1])
     return weights, features, labels
 
 
-def load_random_data(n_samples=500, blobs=False):
-    ''' load some data '''
+def load_random_data(num_dim, n_samples=500, blobs=False):
+    ''' generate data for binary classificaation'''
     if blobs:
-        features, labels = make_blobs(n_samples=n_samples, n_features=13, random_state=1, centers=2)
+        features, labels = make_blobs(n_samples=n_samples, n_features=num_dim,
+                                      random_state=1, centers=2)
     else:
         features, labels = make_classification \
-        (n_samples=n_samples, n_features=13, random_state=1, n_classes=2, flip_y=0.005)
+        (n_samples=n_samples, n_features=num_dim, random_state=1, n_classes=2, flip_y=0.005)
     labels = np.reshape(labels, [labels.shape[0], 1])
     return features, labels
-
-
-def load_dataset(n_samples):
-    ''' create Dataset with given data '''
-    dataset = Dataset(index=np.arange(n_samples), batch_class=MyBatch)
-    dataset.cv_split()
-    return dataset
-
-
-
-def plot_cost(cost_history):
-    ''' Plot cost history '''
-    plt.plot(range(len(cost_history)), cost_history)
-    plt.axis([0, len(cost_history), np.min(cost_history), np.max(cost_history)])
-    plt.show()
-
-
-def plot_log_cost(sess, data, cost_history):
-    ''' Plot cost history and cost of the target together '''
-    x_features = tf.placeholder(tf.float32, [None, None])
-    y_target = tf.placeholder(tf.float32, [None, 1])
-    weights = tf.placeholder(tf.float32, [13, 1])
-
-    log_input = tf.matmul(x_features, weights)
-    cost = tf.reduce_mean(tf.nn.log_poisson_loss(y_target, log_input, compute_full_loss=False))
-
-    origin_loss = sess.run(cost, feed_dict={weights: data[0], x_features: data[1], y_target: data[2]})
-
-    plt.plot(range(len(cost_history)), cost_history)
-    plt.plot(np.linspace(0, len(cost_history), len(cost_history)), [origin_loss]*len(cost_history), '-', color='g')
-    plt.axis([0, len(cost_history), np.min(cost_history), np.max(cost_history)])
-    plt.show()
--- darima_mylzenova/task_01/regression_model.py
+++ darima_mylzenova/task_01/regression_model.py
@@ -2,65 +2,71 @@
 """
 import sys
 
-import numpy as np
-import os
-import blosc
-
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
 sys.path.append("../..")
-from dataset import Batch, action, model, inbatch_parallel
-from dataset.dataset.image import ImagesBatch
 from dataset.dataset.models.tf import TFModel
 
 class RegressionModel(TFModel):
-    def _build(self, *args, **kwargs):
-        model_type = self.get_from_config('model_type', 'linear')
-        print(model_type)
+    """ Regression model
 
-        dim_shape = self.get_from_config('dimension', 0)
-        x_features = tf.placeholder(tf.float32, [None, None], 'input_features')
-        y_target = tf.placeholder(tf.float32, [None, 1], name='targets')
-        
+    ** Configuration **
+
+    inputs : dict
+        dict with keys, for example 'features' and 'labels' (see :meth:`._make_inputs`)
+
+    body : dict
+        dimension : int
+            dimension of features
+        model_type : linear, logistic or poisson
+            type of regression model
+
+    """
+    @classmethod
+    def default_config(cls):
+        config = TFModel.default_config()
+        config['body']['model_type'] = 'linear'
+        return config
+
+    @classmethod
+    def body(cls, inputs, name='body', **kwargs):
+        kwargs = cls.fill_params('body', **kwargs)
+        model_type = cls.pop('model_type', kwargs)
+        print('model_type = {}'.format(model_type))
+        dim_shape = cls.pop('dimension', kwargs)
+
         weights = tf.Variable(tf.ones([dim_shape, 1]), name='weights')
-
         bias = tf.Variable(tf.ones([1]), name='bias')
-
-        wx_b = tf.add(tf.matmul(x_features, weights), bias)
+        wx_b = tf.add(tf.matmul(inputs, weights), bias)
 
 
         if model_type == 'logistic':
             logit = tf.sign(wx_b, name='predictions')
             y_cup = tf.sign(wx_b, name='predicted_value')
-
+            y_target = tf.get_default_graph().get_tensor_by_name('RegressionModel/inputs/targets:0')
             margin = tf.multiply(y_target, wx_b)
             cost = tf.reduce_mean(tf.add(tf.log(tf.add(tf.ones([1]), tf.exp(-margin))), \
-                tf.multiply(tf.reduce_sum(tf.square(weights)), 0.1)))
+                                  tf.multiply(tf.reduce_sum(tf.square(weights)), 0.1)))
             tf.losses.add_loss(cost)
+
             y_cup_int = tf.cast(y_cup, tf.int32)
             y_target_int = tf.cast(y_target, tf.int32)
-            accuracy = tf.contrib.metrics.accuracy(y_cup_int, y_target_int, name='acc_0') 
-            accy = tf.identity(accuracy, name='accuracy')
-
-
+            accy = tf.contrib.metrics.accuracy(y_cup_int, y_target_int, name='accuracy_0')
+            accuracy = tf.identity(accy, name='accuracy')
 
         elif model_type == 'poisson':
-            logit = tf.identity(wx_b, name='predictions')
-            y_cup = tf.cast(tf.exp(logit), tf.int32, name='predicted_value')
-
-            cost = tf.reduce_mean(tf.add(tf.nn.log_poisson_loss(y_target, logit, compute_full_loss=False), \
-                tf.multiply(tf.reduce_sum(tf.square(weights)), 0.01)))
-            tf.losses.add_loss(cost)
-
+            pass
 
         elif model_type == 'linear':
             logit = tf.identity(wx_b, name='predictions')
             y_cup = tf.identity(logit, name='predicted_value')
+            y_target = tf.get_default_graph().get_tensor_by_name('RegressionModel/inputs/targets:0')
             mse = tf.reduce_mean(tf.square(y_cup - y_target), name='mse')
 
 
         else:
-            print('you assigned model_type as %s ' % model_type, 'while model_type must be linear, logistic or poisson')
+            print('you assigned model_type as %s ' % model_type,
+                  'while model_type must be linear, logistic or poisson')
             raise Exception
 
+        return wx_b
