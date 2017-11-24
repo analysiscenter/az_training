4a74703fe8bf1d5f62b995c36b60aca2b7204a3f
anton-br <ziliboba753@gmail.com>
2017-11-24 19:47:29 +0300

    fix pylint error

--- anton_broilovskiy/task_08/resnet_with_stochastic_depth.py
+++ anton_broilovskiy/task_08/resnet_with_stochastic_depth.py
@@ -50,8 +50,9 @@ class StochasticResNet(ResNet50):
                         strides = 2 if i > 0 and block == 0 else 1
                         off = tf.cond(kwargs['is_training'], \
                                       lambda: tf.where(tf.random_uniform([1], 0, 1) > (1 - prob[global_block]),
-                                       tf.ones([1]), tf.zeros([1])), lambda: tf.ones([1]) * prob[global_block])[0]
-                        x = cls.block(x, filters=filters[i], name='layer-%d' % block, off=off, 
+                                                       tf.ones([1]), tf.zeros([1])),
+                                                       lambda: tf.ones([1]) * prob[global_block])[0]
+                        x = cls.block(x, filters=filters[i], name='layer-%d' % block, off=off,
                                       strides=strides, **block_args)
                         global_block += 1
         return x
