69a95d06e8eb990ace0304f2fdab5c6dd83f74d6
a.kozhevin <a.kozhevin@analysiscenter.ru>
2017-12-04 19:05:19 +0300

    delete actions for prototype

--- alexey_kozhevin/task_03/detection_mnist.py
+++ alexey_kozhevin/task_03/detection_mnist.py
@@ -8,172 +8,15 @@ import time
 from scipy.special import expit
 import scipy.ndimage
 import itertools
-from skimage.transform import resize 
-
-sys.path.append('../az_training')
+from skimage.transform import resize
 
 from dataset import action, inbatch_parallel, any_action_failed
 from dataset import ImagesBatch
 
-_IOU_LOW = 0.3
-_IOU_HIGH = 0.7
-
-class FasterRCNNBatch(ImagesBatch):
-    """Batch class for Faster RCNN."""
-
-    components = ('images', 'labels', 'bboxes',
-                  'anchors', 'reg', 'clsf', 'anchor_labels', 'anchor_batch')
-    
-    @action
-    def create_anchors(self, image_shape, scales=(4, 8, 16), ratio=2):
-        """ Create anchors for image_shape depending on output_map_shape. """
-        map_shape = self.pipeline.config['rpn']['output_map_shape']
-        ratios = ((np.sqrt(ratio), 1/np.sqrt(ratio)),
-                  (1, 1),
-                  (1/np.sqrt(ratio), np.sqrt(ratio)))
-
-        self.anchors = []
-        for scale in scales:
-            for ratio in ratios:
-                ih, iw = image_shape
-                fh, fw = map_shape
-                n = fh * fw
-
-                j = np.array(list(range(fh)))
-                j = np.expand_dims(j, 1)
-                j = np.tile(j, (1, fw))
-                j = j.reshape((-1))
-
-                i = np.array(list(range(fw)))
-                i = np.expand_dims(i, 0)
-                i = np.tile(i, (fh, 1))
-                i = i.reshape((-1))
-
-                s = np.ones((n)) * scale
-                r0 = np.ones((n)) * ratio[0]
-                r1 = np.ones((n)) * ratio[1]
-
-                h = s * r0
-                w = s * r1
-                y = (j + 0.5) * ih / fh - h * 0.5
-                x = (i + 0.5) * iw / fw - w * 0.5
-
-                y, x = [np.maximum(vector, np.zeros((n))) for vector in [y, x]]
-                h = np.minimum(h, ih-y)
-                w = np.minimum(w, iw-x)
-
-                anchors = [np.expand_dims(vector, 1) for vector in [y, x, h, w]]
-                anchors = np.concatenate(anchors, axis=1)
-                self.anchors.append(np.array(anchors, np.int32))
-
-        self.anchors = np.array(self.anchors).transpose(1, 0, 2).reshape(-1, 4)
-        return self
-
-    @action
-    @inbatch_parallel(init='indices', post='assemble', components=('reg', 'clsf', 'anchor_labels'))
-    def create_rpn_inputs(self, ind):
-        """ Create reg and clsf targets of RPN. """
-        anchors = self.anchors
-        bboxes = self.get(ind, 'bboxes')
-        labels = self.get(ind, 'labels')
-
-        n = anchors.shape[0]
-        k = bboxes.shape[0]
-
-        # Compute the IoUs of the anchors and ground truth boxes
-        tiled_anchors = np.tile(np.expand_dims(anchors, 1), (1, k, 1))
-        tiled_bboxes = np.tile(np.expand_dims(bboxes, 0), (n, 1, 1))
-
-        tiled_anchors = tiled_anchors.reshape((-1, 4))
-        tiled_bboxes = tiled_bboxes.reshape((-1, 4))
-
-        ious = self.iou_bbox(tiled_anchors, tiled_bboxes)[0]
-        ious = ious.reshape(n, k)
-
-        # Label each anchor based on its max IoU
-        max_ious = np.max(ious, axis=1)
-        best_bbox_for_anchor = np.argmax(ious, axis=1)
-
-        reg = bboxes[best_bbox_for_anchor]
-        proposal_bboxes_labels = labels[best_bbox_for_anchor].reshape(-1)
-
-        # anchor has at least one gt-bbox with IoU >_IOU_HIGH
-        clsf = np.array(max_ious > _IOU_HIGH, dtype=np.int32)
-
-        # anchor intersects with at least one bbox 
-        best_anchor_for_bbox = np.argmax(ious, axis=0)
-        clsf[best_anchor_for_bbox] = 1
-
-        # max IoU for anchor < _IOU_LOW
-        clsf[np.logical_and(max_ious < _IOU_LOW, clsf == 0)] = -1      
-        return reg, clsf, proposal_bboxes_labels
-
-    @action
-    @inbatch_parallel(init='indices', post='assemble', components='anchor_batch')
-    def batch_anchors(self, ind, batch_size=64):
-        """ Create batch indices for anchors. """
-        clsf = self.get(ind, 'clsf')
-        batch_size = min(batch_size, len(clsf))
-        positive = clsf == 1
-        negative = clsf == -1
-        if sum(positive) + sum(negative) < batch_size:
-            batch_size = sum(positive) + sum(negative)
-        if sum(positive) < batch_size / 2:
-            positive_batch_size = sum(positive)
-            negative_batch_size = batch_size - sum(positive)
-        elif sum(negative) < batch_size / 2:
-            positive_batch_size = batch_size - sum(negative)
-            negative_batch_size = sum(negative)
-        else:
-            positive_batch_size = batch_size // 2
-            negative_batch_size = batch_size // 2
-
-        p = positive / sum(positive)
-        positive_batch = np.random.choice(len(clsf), size=positive_batch_size, replace=False, p=p)
-        p = negative / sum(negative)
-        negative_batch = np.random.choice(len(clsf), size=negative_batch_size, replace=False, p=p)
-        anchor_batch = np.array([False]*len(clsf))
-        anchor_batch[positive_batch] = True
-        anchor_batch[negative_batch] = True
-        return anchor_batch
-
-    @action
-    def transform_clsf(self):
-        """ Convert three classes of anchors to two classes of positive and others. """
-        self.clsf = np.array(self.clsf == 1, dtype=np.int32)
-        return self
-
-    @classmethod
-    def iou_bbox(cls, bboxes1, bboxes2):
-        """ Compute the IoUs between bounding boxes. """
-        bboxes1 = np.array(bboxes1, np.float32)
-        bboxes2 = np.array(bboxes2, np.float32)
-
-        intersection_min_y = np.maximum(bboxes1[:, 0], bboxes2[:, 0])
-        intersection_max_y = np.minimum(bboxes1[:, 0] + bboxes1[:, 2] - 1, bboxes2[:, 0] + bboxes2[:, 2] - 1)
-        intersection_height = np.maximum(intersection_max_y - intersection_min_y + 1, np.zeros_like(bboxes1[:, 0]))
-
-        intersection_min_x = np.maximum(bboxes1[:, 1], bboxes2[:, 1])
-        intersection_max_x = np.minimum(bboxes1[:, 1] + bboxes1[:, 3] - 1, bboxes2[:, 1] + bboxes2[:, 3] - 1)
-        intersection_width = np.maximum(intersection_max_x - intersection_min_x + 1, np.zeros_like(bboxes1[:, 1]))
-
-        area_intersection = intersection_height * intersection_width
-        area_first = bboxes1[:, 2] * bboxes1[:, 3]
-        area_second = bboxes2[:, 2] * bboxes2[:, 3]
-        area_union = area_first + area_second - area_intersection
-
-        iou = area_intersection * 1.0 / area_union
-        iof = area_intersection * 1.0 / area_first
-        ios = area_intersection * 1.0 / area_second
-
-        return iou, iof, ios
-
-
-class DetectionMnist(FasterRCNNBatch):
+class DetectionMnist(ImagesBatch):
     """Batch class for multiple MNIST."""
 
-    components = ('images', 'labels', 'bboxes',
-                  'anchors', 'reg', 'clsf', 'anchor_labels', 'anchor_batch')
+    components = ('images', 'labels', 'bboxes')
 
     @action
     @inbatch_parallel(init='indices', post='post_func_multi')
@@ -217,5 +60,3 @@ class DetectionMnist(FasterRCNNBatch):
             self.labels = labels
             self.bboxes = bboxes
             return self
-
-
