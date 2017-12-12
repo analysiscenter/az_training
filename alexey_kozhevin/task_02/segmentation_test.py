108df50b5e3e63d2800403ea0da59c2a10c3f43e
a.kozhevin <a.kozhevin@analysiscenter.ru>
2017-12-12 18:18:03 +0300

    update notebook

--- alexey_kozhevin/task_02/segmentation_test.py
+++ alexey_kozhevin/task_02/segmentation_test.py
@@ -1,3 +1,5 @@
+# pylint: skip-file
+
 import sys
 from time import time
 import numpy as np
@@ -6,107 +8,104 @@ import tensorflow as tf
 
 sys.path.append('..')
 
-from dataset import Pipeline, DatasetIndex, Dataset, B, V
+from dataset import Pipeline, B, V
 
-from dataset.opensets import MNIST
-from dataset.models.tf import UNet, LinkNet, FCN32, FCN8
-from noised_mnist import NoisedMnist                                          # Batch subclass with loading and noise actions
+from dataset.dataset.opensets import MNIST
+from dataset.dataset.models.tf import UNet, LinkNet, FCN32, FCN8
+from noised_mnist import NoisedMnist
 
-IMAGE_SIZE = 64     # image size
-MNIST_SIZE = 65000  # MNIST database size
-BATCH_SIZE = 32     # batch size for NN training
-MAX_ITER = 500      # number of iterations for NN training
+IMAGE_SIZE = 64
+MNIST_SIZE = 65000
+BATCH_SIZE = 32
+MAX_ITER = 500
 DATA_FORMAT = 'channels_last'
 
 
-def binary_iou(masks, predictions, data_format='channels_last'):
+def _binary_iou(masks, predictions, data_format='channels_last'):
     ind = np.index_exp[:, :, :, 1] if data_format == 'channels_last' else np.index_exp[:, 1, :, :]
     predictions = predictions[ind]
     intersection = np.sum(np.logical_and((predictions > 0.5), masks))
     union = np.sum(np.logical_or((predictions > 0.5), masks))
     return intersection / union
 
-def loss(labels, logits):
+def _loss(labels, logits):
     labels = tf.transpose(labels, [0, 2, 3, 1])
     logits = tf.transpose(logits, [0, 2, 3, 1])
     res = tf.losses.softmax_cross_entropy(labels, logits)
     return res
 
 def demonstrate_model(model, mnistset, data_format='channels_last'):
-    level = 1           # the highest level of noise; [0, 1]
-    n_fragments = 80    # number of noise fragments per image  
-    size = 4            # size of noise fragment; 1, ..., 27
-    distr = 'uniform'   # distribution of fragments of image; 'uniform' or 'normal'
+    """ Test function. """
+    level = 1
+    n_fragments = 80
+    size = 4
+    distr = 'uniform'
 
     shape = (1, IMAGE_SIZE, IMAGE_SIZE) if data_format == 'channels_first' else (IMAGE_SIZE, IMAGE_SIZE, 1)
 
-    placeholders_config = {
-                           'images': {'shape': shape,
+    placeholders_config = {'images': {'shape': shape,
                                       'type': 'float32',
                                       'data_format': data_format,
                                       'name': 'reshaped_images'},
-                    
                            'masks': {'shape': (IMAGE_SIZE, IMAGE_SIZE),
                                      'type': 'int32',
                                      'transform': 'ohe',
                                      'data_format': data_format,
                                      'classes': 2,
-                                     'name': 'targets'}
-                           }
+                                     'name': 'targets'}}
 
     model_config = {'inputs': placeholders_config,
                     'input_block/inputs': 'images',
                     'batch_norm': {'momentum': 0.1},
                     'output': dict(ops=['proba']),
-                    'loss': 'ce' if data_format == 'channels_last' else loss,
+                    'loss': 'ce' if data_format == 'channels_last' else _loss,
                     'optimizer': 'Adam'}
 
     train_feed_dict = {'images': B('images'),
-                       'masks': B('masks')}        
+                       'masks': B('masks')}
 
     test_feed_dict = {'images': B('images'),
                       'masks': B('masks')}
     print('Create pipelines...')
 
     load_template = (Pipeline()
-                 .random_location(IMAGE_SIZE)      # put MNIST at random location
-                 .make_masks()                     # create mask for MNIST image location
-                 .create_noise('mnist_noise', level, n_fragments, size, distr)
-                 .add_noise())
+                     .random_location(IMAGE_SIZE)      # put MNIST at random location
+                     .make_masks()                     # create mask for MNIST image location
+                     .create_noise('mnist_noise', level, n_fragments, size, distr)
+                     .add_noise())
     if data_format == 'channels_first':
         load_template = load_template.swap_axis()
     
     ppl_train = ((load_template << mnistset.train)                         # load data from file
-            .init_model('static', model, 'NN', config=model_config)
-            .init_variable('loss', init_on_each_run=list)
-            .train_model('NN',
-                         fetches='loss',
-                         feed_dict=train_feed_dict,
-                         save_to=V('loss'), mode='a'))
+                 .init_model('static', model, 'NN', config=model_config)
+                 .init_variable('loss', init_on_each_run=list)
+                 .train_model('NN',
+                              fetches='loss',
+                              feed_dict=train_feed_dict,
+                              save_to=V('loss'), mode='a'))
 
     print('Start training...')
     start = time()
     for i in range(MAX_ITER):
         ppl_train.next_batch(BATCH_SIZE, n_epochs=None, shuffle=True)
-        #print(ppl_train.get_variable('loss')[-1])
     stop = time()
     print("Train time: {:05.3f} min".format((stop-start)/60))
 
     ppl_test = ((load_template << mnistset.test)
-             .import_model('NN', ppl_train)
-             .init_variable('predictions', init_on_each_run=list)
-             .predict_model('NN',                                      
-                           fetches='predicted_proba',
-                           feed_dict=test_feed_dict,
-                           save_to=V('predictions'),
-                           mode='a'))
+                .import_model('NN', ppl_train)
+                .init_variable('predictions', init_on_each_run=list)
+                .predict_model('NN',
+                               fetches='predicted_proba',
+                               feed_dict=test_feed_dict,
+                               save_to=V('predictions'),
+                               mode='a'))
 
     batch = ppl_test.next_batch(100, n_epochs=None)
     images = batch.data.images
     masks = batch.data.masks
     noise = batch.data.noise
     predictions = ppl_test.get_variable('predictions')[-1]
-    iou = binary_iou(masks, predictions, data_format)
+    iou = _binary_iou(masks, predictions, data_format)
 
     print(model.__name__, 'Test IoU: {0:.3f}'.format(iou))
 
