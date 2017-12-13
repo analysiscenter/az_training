a7521a80963075a4b2806edafb25cdfb56bbd9f0
anton-br <ziliboba753@gmail.com>
2017-12-13 21:42:15 +0300

    update batch_generator

--- anton_broilovskiy/task_11/batch_generator.py
+++ anton_broilovskiy/task_11/batch_generator.py
@@ -7,7 +7,7 @@ from dataset.dataset import ImagesBatch, action, inbatch_parallel
 class TwoMnistBatch(ImagesBatch):
     """ Batch class which create colorize image """
 
-    components = 'images', 'labels', 'color', 'first_number', 'second_number', 'indices'
+    components = 'images', 'labels', 'first_color', 'second_color', 'first_number', 'second_number', 'indices'
 
     @action
     def normalize_images(self):
@@ -16,34 +16,46 @@ class TwoMnistBatch(ImagesBatch):
         return self
 
     @action
-    @inbatch_parallel(init='init_func', post='assemble', components=['images', 'color', 'first_number',
-                                                                     'second_number'])
-    def concat_and_colorize_images(self, ind):
-        """ From a black and white image makes either a red or blue image
-        with probability = percent_blue.
-        Parameters
-        ----------
-        image : np.array
-            input image
-        Returns
-        -------
-            colorized image
-        """
+    @inbatch_parallel(init='init_f', post='assemble', components=['images', 'first_color', 'second_color',\
+                     'first_number', 'second_number'])
+    def colorize_images(self, ind, colors):
+        """ colorize images"""
+        image = self.get(ind, 'images')
+        shape = image.shape[:2]
+        image = color.gray2rgb(image).reshape(*shape, 3)
+        ind = np.arange(len(colors))
+        np.random.shuffle(ind)
+        colors = np.array(colors)[ind]
+
+        label = self.get(ind, 'labels')
+        if image.shape[1] <= image.shape[0]:
+            label = np.hstack(([label], [-1]))
+            return [image * colors, *[colors]*2, *label]
+
+        colorized_image = np.hstack((image[:, :int(shape[1]/2)] * colors[0], image[:, int(shape[1]/2):] * colors[1]))
+    return [colorized_image, *ind, *label]
+
+    @action
+    @inbatch_parallel(init='init_func', post='assemble', components=['images', 'labels'])
+    def gluing_of_images(self, ind):
+        """gluing images """
         image = self.get(ind, 'images')
         label = self.get(ind, 'labels')
-        shape = image.shape[1:3]
-        image = color.gray2rgb(image).reshape(-1, *shape, 3)
 
-        indices = np.array([0, 1])
-        np.random.shuffle(indices)
-        col = np.array([[0., 0., 1.], [1., 0., 0.]])[indices]
-
-        return [np.hstack((image[0] * col[0], image[1] * col[1])), indices[0], *label]
+        return [np.hstack((image[0], image[1])), label]
 
     def init_func(self, components, **kwargs):
         """ Create queue to parallel.
         Resurns
         -------
             Array with parallel indices """
-        _ = components
-        return [{'ind':[i, np.random.choice(self.indices)]} for i in self.indices]
+        _ = components, kwargs
+        return [{'ind':np.array([i, np.random.choice(self.indices)])} for i in self.indices]
+
+    def init_f(self, components, **kwargs):
+        """ Create queue to parallel.
+        Resurns
+        -------
+            Array with parallel indices """
+        _ = components, kwargs
+        return [{'ind':i} for i in self.indices]
