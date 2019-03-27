35f4c2a3d17785c2da50bdf34e4ab525c533fbcc
nikita <nikita_007_94@mail.ru>
2019-03-27 22:55:54 +0300

    segmentation mnist 0.72 iou

--- nikita_kalashnikov/Segmentation/batch.py
+++ nikita_kalashnikov/Segmentation/batch.py
@@ -5,34 +5,57 @@ from batchflow import action, inbatch_parallel
 from batchflow.batch_image import transform_actions
 
 @transform_actions(prefix='_', suffix='_', wrapper='apply_transform')
-@transform_actions(prefix='_', suffix='_all', wrapper='apply_transform_all')
 class MyBatch(ImagesBatch):
     """ Batch class for segmentation task.
     """
     components = 'images', 'masks'
         
     @action
-    @inbatch_parallel(init='indices')
+    @inbatch_parallel(init='indices', target='for')
     def noise(self, ind, n=10):
-        index = self.get_pos(None, None, ind)
-        image_to = self.images[index]
-        size = image_to.width
+        i = self.get_pos(None, None, ind)
+        size = self.images[i].width 
         for image in self.images:
             for _ in range(n):
                 shape = np.random.randint(3, 7, size=2)
                 x_to, y_to = np.random.randint(0, size-max(*shape), size=2)
                 crop = self._crop_(image, origin='random', shape=shape)
-                self.images[index].paste(crop, (x_to,y_to))
-        
-
+                self.images[i].paste(crop, (x_to,y_to))
 
     @action
-    @inbatch_parallel(init='images',post='post_fn')
-    def mask(self, image):
-        return  (np.array(image) > 0)[:,:,0].astype(int)
+    @inbatch_parallel(init='indices', post='post_fn')
+    def background_and_mask(self, ind, bg_shape=(128,128)):
+        i = self.get_pos(None, None, ind)
+        image = self.images[i].convert(mode='RGB')
+        background = PIL.Image.fromarray(np.zeros(bg_shape), mode='RGB')
+        shape = image.size
+        x, y = self._calc_origin(image_shape=shape, origin='random', background_shape=bg_shape)
+        background.paste(image, (x,y))
+        self.images[i] = background
+        mask = np.zeros(bg_shape)
+        mask[y:y+shape[0]+1, x:x+shape[1]+1] = 1
+        #mask = np.array(background) > 0
+        return mask.astype('long')
     
-    def post_fn(self, list_of_res):
-        self.mask = np.array(list_of_res)
+    def post_fn(self, list_of_res, *args):
+        self.masks = np.array(list_of_res)
         return self
 
+    def _preprocess_images_(self, image):
+        """ Reshape images and cast arrays to type float32.
+
+        Parameters
+        ----------
+        image : PIL.Image
+            Image in the batch.
+
+        Returns
+        -------
+        image_reshape : np.array
+            Reshaped image with channels dimension first.
+        """
+        shape = tuple(reversed(np.array(image).shape))
+        image_reshape = np.array(image).reshape(shape).astype('float32')
+        return image_reshape
+                
 
--- nikita_kalashnikov/Segmentation/utils.py
+++ nikita_kalashnikov/Segmentation/utils.py
@@ -7,8 +7,24 @@ def plot_augm_masks(batch):
     _, axis = plt.subplots(nrows=2, ncols=5, figsize=(17, 17))
     plt.subplots_adjust(top=0.5, bottom=0.1, left=0., right=1,
                         hspace=0.3, wspace=0.2)
-    iter = zip(axis.flatten(), chain(batch.images[:5], batch.mask[:5]))
+    iter = zip(axis.flatten(), chain(batch.images[:5], batch.masks[:5]))
     for ax, image in iter:
+        if len(image.shape) > 2:
+            w = image.shape[1]
+            image = image.reshape(w, w, -1)
+            image /= 255
         ax.axis('off')
-        ax.imshow(np.array(image))
+        ax.imshow(image)
+
+def plot_pred_mask(test_batch, pred):
+    w = test_batch.images[0].shape[1]
+    _, axis = plt.subplots(nrows=3, ncols=3, figsize=(17, 17))
+    iterator = chain(test_batch.images[:3], test_batch.masks[:3], pred[:3])
+    for ax, image in zip(axis.flatten(), iterator):
+        if image.shape[0] == 3:
+            image = image.reshape(w, w, 3) / 255
+        elif image.shape[0] == 2:
+            image = np.where(image[1,:,:] > image[0,:,:], 1, 0)
+            #image = image[0]
+        ax.imshow(image)
 
